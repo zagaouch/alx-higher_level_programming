a34a25b326d7060e7f4adbf835bcb4fe7d3615cf
zagaouch <i.zagaouch@gmail.com>
2023-08-12 13:00:13 +0100

    omg C again

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * check_cycle - check if the liste is circl
+ * @list : the list
+ * Return: 1 or 0
+*/
+int check_cycle(listint_t *list)
+{
+    listint_t *slow = list;
+    listint_t *fast = list;
+
+    while (slow != NULL && fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast)
+        {
+            return 1;
+        }
+    }
+    return(0);
+}
