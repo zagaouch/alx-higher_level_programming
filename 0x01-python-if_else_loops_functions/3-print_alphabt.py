b7cb12235795d648d0dc3cc6a4d5b5038ad86a80
zagaouch <i.zagaouch@gmail.com>
2023-08-27 14:19:11 +0100

    task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for char_code in range(97, 123):
-    if char_code != 112 and char_code != 113:
+    if char_code != 101 and char_code != 113:
         print("{:s}".format(chr(char_code)), end='')
