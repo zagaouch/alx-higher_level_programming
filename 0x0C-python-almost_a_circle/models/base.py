eef26c830ee585719298c9b3d067bb8c0fec4968
zagaouch <i.zagaouch@gmail.com>
2023-09-18 23:05:58 +0100

    taske

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+""" An empty class base"""
 
 
 class Base:
+    """defines a class base that has a private attribute"""
     __nb_objects = 0
 
     def __init__(self, id=None):
