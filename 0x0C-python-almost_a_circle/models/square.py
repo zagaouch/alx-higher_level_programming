d3728d8aaac336706d9ff0884d5c909f9ea9c43f
zagaouch <i.zagaouch@gmail.com>
2023-09-20 23:05:19 +0100

    square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,65 +1,31 @@
 #!/usr/bin/python3
-"""Module for Square class."""
+"""creates a function Rectangle"""
+
+
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Represents a Square, which is a special Rectangle."""
+    """defines a square"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """Initializes a Square instance.
-
-        Args:
-            size (int): The size (width/height) of the square.
-            x (int): The x-coordinate of the square's position.
-            y (int): The y-coordinate of the square's position.
-            id (int): The identifier of the square.
-
-        """
+        """takes in similar arguments to rectangle"/
+        "replaces width and height with size"""
         super().__init__(size, size, x, y, id)
-
-    def __str__(self):
-        """Returns a custom string representation of the Square instance."""
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
 
     @property
     def size(self):
-        """Getter for the size attribute (same as width)."""
         return self.width
 
     @size.setter
     def size(self, value):
-        """Setter for the size attribute (same as width and height)."""
         self.width = value
         self.height = value
 
-    def update(self, *args, **kwargs):
-        """Updates attributes based on no-keyword arguments or keyword arguments."""
-        if args:
-            if len(args) >= 1:
-                self.id = args[0]
-            if len(args) >= 2:
-                self.size = args[1]
-            if len(args) >= 3:
-                self.x = args[2]
-            if len(args) >= 4:
-                self.y = args[3]
-        else:
-            for key, value in kwargs.items():
-                if key == 'id':
-                    self.id = value
-                elif key == 'size':
-                    self.size = value
-                elif key == 'x':
-                    self.x = value
-                elif key == 'y':
-                    self.y = value
-
-    def to_dictionary(self):
-        """Returns the dictionary representation of the Square instance."""
-        return {
-            'id': self.id,
-            'size': self.size,
-            'x': self.x,
-            'y': self.y
-        }
+    def __str__(self):
+        """
+            string representation of Square class instance
+        """
+        return "[{}] ({}) {}/{} - {}" \
+            .format(self.__class__.__name__, self.id,
+                    self.x, self.y, self.size)
