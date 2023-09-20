ac32e33e603b22000f0d2010787cb51275ba80ce
zagaouch <i.zagaouch@gmail.com>
2023-09-20 23:06:59 +0100

    square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -29,3 +29,25 @@ class Square(Rectangle):
         return "[{}] ({}) {}/{} - {}" \
             .format(self.__class__.__name__, self.id,
                     self.x, self.y, self.size)
+
+    def update(self, *args, **kwargs):
+        """updates propeties of a class instance"""
+        if len(args):
+            for i, a in enumerate(args):
+                if i == 0:
+                    self.id = a
+                elif i == 1:
+                    self.size = a
+                elif i == 2:
+                    self.x = a
+                elif i == 3:
+                    self.y = a
+        else:
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "size" in kwargs:
+                self.size = kwargs["size"]
+            if "x" in kwargs:
+                self.x = kwargs["x"]
+            if "y" in kwargs:
+                self.y = kwargs["y"]
