a3cd52a2be78f8fe299d43217f6dd1bcd1a7942d
zagaouch <i.zagaouch@gmail.com>
2023-09-22 15:13:04 +0100

    square class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,7 +4,7 @@
 
 class Base:
     """defines a class base that has a private attribute"""
-    __nb_objects = 1
+    __nb_objects = 0
 
     def __init__(self, id=None):
         if id is not None:
