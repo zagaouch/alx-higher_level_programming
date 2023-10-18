fc03b15a73ada5c72b3391006e7000e260ba6384
zagaouch <i.zagaouch@gmail.com>
2023-10-18 18:30:37 +0100

    rest od tasks

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,6 @@
-- display the average temp by city orderd by temp
-SELECT city,AVG(value) AS avg_temp
+-- display the average temp by city orderd by temp
+SELECT city,
+    AVG(value) AS avg_temp
 FROM temperatures
 GROUP BY city
 ORDER BY avg_temp DESC;
