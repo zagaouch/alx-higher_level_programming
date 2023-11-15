40ceac9d14c4d42bfb79d4cd2f3fe9ce8199bcb9
zagaouch <i.zagaouch@gmail.com>
2023-11-16 00:58:44 +0100

    task 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const argvs = process.argvs;
+if (argvs === 0) {
+  console.log('No argument');
+}
+if (argvs === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Argument found');
+}
