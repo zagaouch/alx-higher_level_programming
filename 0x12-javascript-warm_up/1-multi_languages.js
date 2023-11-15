02f4da26cc535d32710d728c3e033e439fe8aa96
zagaouch <i.zagaouch@gmail.com>
2023-11-16 00:44:15 +0100

    task 0 1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,8 @@
 #!/usr/bin/node
-const myVar = 'C is fun \nPython is cool \navaScript is amazing';
-console.log(myVar);
+const Astr = 'C is fun';
+const Bstr = 'Python is cool';
+const Cstr = 'JavaScript is amazing';
+
+console.log(Astr); // console.log(Astr) will print 'C is fun'
+console.log(Bstr); // console.log(Bstr) will print 'Python is cool'
+console.log(Cstr); // console.log(Cstr) will print 'JavaScript is amazing'
