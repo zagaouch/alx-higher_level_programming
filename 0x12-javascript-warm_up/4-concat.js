23d3fb4334e30219d1a07ebfa2f3108c7c1a78bc
zagaouch <i.zagaouch@gmail.com>
2023-12-09 21:34:33 +0100

    retuen

--- 0x12-javascript-warm_up/4-concat.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/node
-const args = process.argv; // Array of arguments
-
-console.log(args[2] + ' is ' + args[3]); // print somthing is somthing
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-const arr = [];
-arr.push('C is fun','Python is cool','JavaScript is amazing');
-
- 
