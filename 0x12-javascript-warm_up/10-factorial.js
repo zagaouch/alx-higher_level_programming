1234d478bdba308f5aa366213c5644f0215a370d
zagaouch <i.zagaouch@gmail.com>
2024-01-08 22:39:27 +0100

    javascript is the best

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-function factorial (n) { // n is the number to be factorialized
-  if (!n) { // if n is 0, return 1
-    return 1;
+function factorial (n) {
+    if (!n) {
+      return 1;
+    }
+    return n * factorial(n - 1);
   }
-  return n * factorial(n - 1); // otherwise, return n * the factorial of n - 1
-}
-
-console.log(factorial(process.argv[2])); // print the factorial of the number passed in as an argument
+  
+  console.log(factorial(process.argv[2]));
--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-exports.callMeMoby = function (x, theFunction) { // This function calls a function passed 'x' times
+exports.callMeMoby = function (x, theFunction) {
   for (let i = 0; i < x; i++) {
     theFunction();
   }
