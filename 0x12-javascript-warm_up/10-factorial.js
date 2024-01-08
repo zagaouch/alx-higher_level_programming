9a5bf9e35a61558cf65a1c66f96082ee2396f2ab
zagaouch <i.zagaouch@gmail.com>
2024-01-08 22:37:04 +0100

    javascript is the best

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 function factorial (n) { // n is the number to be factorialized
-    if (!n) { // if n is 0, return 1
-      return 1;
-    }
-    return n * factorial(n - 1); // otherwise, return n * the factorial of n - 1
+  if (!n) { // if n is 0, return 1
+    return 1;
   }
-  
-  console.log(factorial(process.argv[2])); // print the factorial of the number passed in as an argument
+  return n * factorial(n - 1); // otherwise, return n * the factorial of n - 1
+}
+
+console.log(factorial(process.argv[2])); // print the factorial of the number passed in as an argument
