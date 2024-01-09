2afdabe9452b87d836edce2740bfe65ac260318f
zagaouch <i.zagaouch@gmail.com>
2024-01-09 22:13:48 +0100

    task3

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
     constructor (w, h) {
-        if(w > à && h > 0){
-            this.width = w;
-            this.height = h;
-        }
+      if (w > 0 && h > 0) {
+        this.width = w;
+        this.height = h;
+      }
     }
   };
+  
