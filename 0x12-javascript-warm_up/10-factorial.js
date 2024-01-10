6eb5ee5702cfc1f92ca61a3b230b26ff0d4280f7
zagaouch <i.zagaouch@gmail.com>
2024-01-10 20:29:48 +0100

    fix 12

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 function factorial (n) {
-    if (!n) {
-      return 1;
-    }
-    return n * factorial(n - 1);
+  if (!n) {
+    return 1;
   }
-  
-  console.log(factorial(process.argv[2]));
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(process.argv[2]));
