bac2d64237e01a14ff44cd1085c98765941046c8
zagaouch <i.zagaouch@gmail.com>
2024-01-10 20:49:30 +0100

    fik

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-exports.add = function (a, b) {
-    return a + b;
+exports.add = function (a, b) { // exports is a special object that is used to export functions and variables from a 
+    return a + b; // return the sum of a and b
 };
