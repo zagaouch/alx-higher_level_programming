6e1259588a8aa7c21c35380482899392607013ef
zagaouch <i.zagaouch@gmail.com>
2024-01-14 22:18:04 +0100

    task1é

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-""" Write a script that lists all states with a name starting
-with N (upper N) from the database hbtn_0e_0_usa """
+""" Write a script that takes in an argument and displays all values in the
+states table of hbtn_0e_0_usa where name matches the argument """
 
 import MySQLdb
 import sys
@@ -10,7 +10,8 @@ if __name__ == "__main__":
                           user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = con.cursor()
     cursor.execute(
-        "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+        "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER \
+        BY id ASC".format(sys.argv[4]))
     rows = cursor.fetchall()
     for row in rows:
         print(row)
