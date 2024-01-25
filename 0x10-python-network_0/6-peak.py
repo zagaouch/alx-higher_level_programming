0df6b783a8c27d1b40ba4e10bb70baf8510fbb65
zagaouch <i.zagaouch@gmail.com>
2024-01-25 22:17:16 +0100

    auto commit

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -9,7 +9,8 @@ def find_peak(list_of_integers):
     left, right = 0, len(list_of_integers) - 1
     while left < right:
         mid = (left + right) // 2
-        if list_of_integers[mid] > list_of_integers[mid - 1] and list_of_integers[mid] > list_of_integers[mid + 1]:
+        if (list_of_integers[mid] > list_of_integers[mid - 1]
+            and list_of_integers[mid] > list_of_integers[mid + 1]):
             return mid
         elif list_of_integers[mid] < list_of_integers[mid - 1]:
             right = mid - 1
