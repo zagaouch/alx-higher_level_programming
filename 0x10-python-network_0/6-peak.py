4ff8785d62f56e25da94730e9d28faafc6af06d4
zagaouch <i.zagaouch@gmail.com>
2024-01-25 22:14:09 +0100

    auto commit

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def find_peak(list_of_integers):
+    """
+    Finds a peak in a list of unsorted integers.
+    Complexity:
+        O(log n)
+    """
+
+    left, right = 0, len(list_of_integers) - 1
+    while left < right:
+        mid = (left + right) // 2
+        if list_of_integers[mid] > list_of_integers[mid - 1] and list_of_integers[mid] > list_of_integers[mid + 1]:
+            return mid
+        elif list_of_integers[mid] < list_of_integers[mid - 1]:
+            right = mid - 1
+        else:
+            left = mid + 1
+    return left
