0c9297fef23893175c870c9d77ace3874ef2f75b
zagaouch <i.zagaouch@gmail.com>
2024-02-01 20:07:24 +0100

    auto commit

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import urllib
+
+url = "https://alx-intranet.hbtn.io/status"
+with urllib.request.urlopen(url) as response:
+    body = request.read()
+
+print("Body response:$")
+print("\t- type: {}".format(type(html)))
+print("\t- content: {}".format(html))
+print("\t- utf8 content: {}".format(html.decode('utf8')))
