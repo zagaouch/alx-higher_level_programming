ee36a53cae3e5705d901cabd055c32aa6cce7e33
zagaouch <i.zagaouch@gmail.com>
2024-02-20 20:15:05 +0100

    auto commit

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
 // Read from file
 
-const fs = require('fs');
-const filePath = process.argv[2]; 
-function readFileContent(filePath) {
-    fs.readFile(filePath, 'utf-8', (err, data) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        console.log(data);
-    });
-}
+const filesys = require('fs');
+filesys.readFile(process.argv[2], 'utf-8',
+  function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(data);
+  });
