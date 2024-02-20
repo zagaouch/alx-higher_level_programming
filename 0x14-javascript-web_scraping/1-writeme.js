6e7d2204ca4d1ce9e047e7f1d07da32d55f6c460
zagaouch <i.zagaouch@gmail.com>
2024-02-20 20:27:19 +0100

    auto commit

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,10 +2,9 @@
 // wite from file
 
 const filesys = require('fs');
-filesys.writeFile(process.argv[2], process.argv[3],'utf-8',
+filesys.writeFile(process.argv[2], process.argv[3], 'utf-8',
   function (err, data) {
     if (err) {
       console.log(err);
-      return;
     }
   });
